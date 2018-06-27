406e6ee84a1dab620df6d3ad025fa1b61327b5fa
Cory R Stein <cory.r.stein@us.pwc.com>
2018-06-27 15:23:50 -0400

    Added Nginx

--- scripts/installJira.sh
+++ scripts/installJira.sh
@@ -90,7 +90,7 @@ echo "Successfully installed Java"
 echo "Installing Jira..."
 JIRA_VERSION=7.10.2
 # https://confluence.atlassian.com/adminjiraserver071/unattended-installation-855475683.html
-pushd /tmp > /dev/null
+pushd /tmp >/dev/null
 
 # Create application directory
 TARGET_DIR=/opt/atlassian/jira
@@ -128,15 +128,15 @@ echo "Completed setting permissions"
 # Create home directory
 echo "Create home directory..."
 HOME_DIR=/var/jirasoftware-home
-mkdir -p ${HOME_DIR} > /dev/null
+mkdir -p ${HOME_DIR} >/dev/null
 chown -R jira ${HOME_DIR}
 chmod -R u=rwx,go-rwx ${HOME_DIR}
 echo "Completed creating home directory"
 
 # Set user home for application
 echo "Set user home for application..."
-echo "export JIRA_HOME=${HOME_DIR}" >> /home/jira/.bash_profile
-echo "export JIRA_OPTS=-Datlassian.darkfeature.jira.onboarding.feature.disabled=true" >> /home/jira/.bash_profile
+echo "export JIRA_HOME=${HOME_DIR}" >>/home/jira/.bash_profile
+echo "export JIRA_OPTS=-Datlassian.darkfeature.jira.onboarding.feature.disabled=true" >>/home/jira/.bash_profile
 echo "Completed setting user home for application"
 
 # Configure application ports
@@ -155,7 +155,7 @@ echo "Completed configuring JIRA JVM memory"
 # Create systemd file
 # Ref: https://community.atlassian.com/t5/Jira-questions/CentOS-7-systemd-startup-scripts-for-Jira-Fisheye/qaq-p/157575
 echo "Create systemd file..."
-cat > /usr/lib/systemd/system/jira.service << EOL
+cat >/usr/lib/systemd/system/jira.service <<EOL
 [Unit]
 Description=JIRA Service
 After=network.target
@@ -164,6 +164,7 @@ After=network.target
 Type=forking
 User=jira
 Environment=JIRA_HOME=${HOME_DIR}
+Environment=JIRA_OPTS=-Datlassian.darkfeature.jira.onboarding.feature.disabled=true
 PIDFile=${TARGET_DIR}/work/catalina.pid
 ExecStart=${TARGET_DIR}/bin/start-jira.sh
 ExecStop=${TARGET_DIR}/bin/stop-jira.sh
@@ -184,16 +185,113 @@ systemctl status jira.service
 echo "Completed Jira status"
 echo "Completed creating systemd file"
 
-popd > /dev/null
+popd >/dev/null
 echo "Completed installing Jira"
 ####################################################################
-
 
 ####################################################################
 # Install/Configure Nginx
 ####################################################################
 # https://confluence.atlassian.com/jirakb/integrating-jira-with-nginx-426115340.html
-#yum install -y nginx
+# https://www.digitalocean.com/community/tutorials/how-to-install-nginx-on-centos-7
+echo Adding Nginx repository...
+yum -y install epel-release
+
+echo Installing Nginx...
+yum -y install nginx
+
+# Configure Nginx Sites For
+echo "Creating directories..."
+mkdir -p /etc/nginx/sites-available
+mkdir -p /etc/nginx/sites-enabled
+mkdir -p /var/cache/nginx/client_temp
+chmod 0777 /var/cache/nginx/client_temp
+echo "Completed creating directories"
+
+# Update config file
+echo "Editing [/etc/nginx/nginx.conf]..."
+SEARCH="include \/etc\/nginx\/conf.d\/\*.conf;"
+REPLACE="include \/etc\/nginx\/sites-enabled\/\*.conf;"
+sed -i -e "s|$SEARCH|$REPLACE|g" /etc/nginx/nginx.conf
+sed -i -e "s|        listen       80 default_server;|#        listen       80 default_server;|g" /etc/nginx/nginx.conf
+sed -i -e "s|        listen       \[::\]:80 default_server;|#        listen       \[::\]:80 default_server;|g" /etc/nginx/nginx.conf
+sed -i -e "s|        server_name  _;|#        server_name  _;|g" /etc/nginx/nginx.conf
+sed -i -e "s|        root         /usr/share/nginx/html;|#        root         /usr/share/nginx/html;|g" /etc/nginx/nginx.conf
+echo Return Code: $?
+echo "Completed editing [/etc/nginx/nginx.conf]"
+
+# Remove contents of /etc/nginx/conf.d
+echo "Removing [/etc/nginx/conf.d/*]..."
+rm -f /etc/nginx/conf.d/*
+echo "Completed [/etc/nginx/conf.d/*]"
+
+# HTTP/S Configuration
+SERVER_NAME="localhost"
+DNS="gatt-nexus-oss.pwcinternal.com"
+SERVER_PORT="80"
+cat >/etc/nginx/sites-available/jira.conf <<EOL
+server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        server_name _;
+        return 301 https://\$host\$request_uri;
+}
+
+
+server {
+        #listen 80 default_server;
+        #listen [::]:80 default_server;
+        #server_name localhost;
+
+        # SSL listener
+        #listen 443 ssl;
+        #listen [::]:443 default_server;
+        #server_name ${SERVER_NAME} ${DNS} ;
+
+        # SSL Certificates / Configuration
+        #ssl on;
+        #ssl_certificate     /etc/ssl/${DNS}.cer;
+        #ssl_certificate_key /etc/ssl/${DNS}.key;
+
+        # allow large uploads of files - refer to nginx documentation
+        client_max_body_size 1G;
+        # optimize downloading files larger than 1G - refer to nginx doc before adjusting
+        #proxy_max_temp_file_size 2G;
+
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto "https";
+        location / {
+                proxy_pass http://localhost:8080;
+        }
+}
+
+EOL
+
+# Enable the configuration by creating symbolic link (Incomplete)
+ln -sf /etc/nginx/sites-available/jira.conf /etc/nginx/sites-enabled/jira.conf
+
+# Validate nginx configuration file
+echo "Validating Nginx confiugration file..."
+nginx -t
+echo "Completed validating Nginx confiugration file"
+
+# Allow http and https ports through firewall
+if [ $(systemctl -q is-active firewalld) ]; then
+	firewall-cmd --permanent --zone=public --add-service=http
+	firewall-cmd --permanent --zone=public --add-service=https
+	firewall-cmd --reload
+fi
+
+# Restart Nginx
+echo "Starting Nginx service..."
+systemctl start nginx
+systemctl enable nginx
+# Configure selinux
+setsebool -P httpd_can_network_connect 1
+echo "Completed restarting Nginx service"
+
 ####################################################################
 
 echo "Executing [$0] complete"
